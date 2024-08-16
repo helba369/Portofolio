d45195963d55fca51e5b29af0fa099511ca9f37f
helba369 <deddymichael3@gmail.com>
2024-08-16 14:21:19 +0800

    add contact

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function toggleDetail(e) {
     const target = $(e.target)
 
-    if($(target).hasClass("active")) {
+    if ($(target).hasClass("active")) {
         $(target).html("More Info").removeClass("active")
     } else {
         $(target).html("Less Info").addClass("active")
@@ -14,4 +14,24 @@ function toggleDetail(e) {
 
     // console.log($(item).children(".about-exp-item-detail"))
 
+}
+
+function onFormSubmit(e) {
+    e.preventDefault()
+    const email = $("#inp_email")
+    const subject = $("#inp_subject")
+    const message = $("#inp_message")
+
+    if (!$(email).val()) {
+        alert("Email is Required")
+    } else if (!$(subject).val()) {
+        alert("Subject is Required")
+    } else if (!$(message).val()) {
+        alert("Message is Required")
+    } else {
+        $(email).val("")
+        $(subject).val("")
+        $(message).val("")
+        alert("Form Submitted")
+    }
 }
